0bc305ba90067578d83b24ab08baabf6adb55f35
apatranx <ap47ra@gmail.com>
2023-01-15 00:18:45 +0700

    Update Bug

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit,AfterViewInit,Renderer2 } from '@angular/core';
 
 declare const $ : any;
 declare const Plugin : any;
@@ -9,22 +9,20 @@ declare const Plugin : any;
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  
-  constructor(private renderer : Renderer2) { }
 
+  constructor(private renderer : Renderer2) { }
   ngAfterViewInit():void
   {
-    this.renderer.removeClass(document.body,"login-page");
-    this.renderer.removeClass(document.body, "sidebar-open");
-    this.renderer.addClass(document.body, "sidebar-closed");
-
-
-    $('[data-widget="treeview"]').treeview("init");
-    Plugin.call($(this))
+  this.renderer.removeClass(document.body,"login-page");
+  this.renderer.removeClass(document.body,"sidebar-open");
+  this.renderer.addClass(document.body,"sidebar-close");
+  $('[data-widget="treeview"]').treeview("init");
+  Plugin.call($(this))
   }
+  
+
 
   ngOnInit(): void {
-
   }
 
 }
